e9b0783502ea6b173f4aa27dea58c66b798c84e2
Atul Acharya <atul.acharya@gmail.com>
2017-08-23 21:49:08 -0700

    updated: re-did commit

--- BigTipper/SettingsViewController.swift
+++ BigTipper/SettingsViewController.swift
@@ -9,27 +9,95 @@
 import UIKit
 
 class SettingsViewController: UIViewController {
+    
+    @IBOutlet weak var tipControl: UISegmentedControl!
+    
+    @IBOutlet weak var slider: UISlider!
 
+    // MARK: - Lifecycle
+    
+    override func viewWillAppear(_ animated: Bool) {
+        view.backgroundColor = UIColor(red: 91/255.0, green: 184/255.0, blue: 90/255.0, alpha: 1.0)
+        
+        // 1 - 
+        let defaults = UserDefaults.standard
+        
+        if let selectedIndex = defaults.value(forKey: "selectedIndex") as? Int {
+            tipControl.selectedSegmentIndex = selectedIndex
+        } else {
+            tipControl.selectedSegmentIndex = 1 // 18%
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        self.updatePreferredContentSizeWithTraitCollection(self.traitCollection)
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    override func willTransition(to newCollection: UITraitCollection, with coordinator: UIViewControllerTransitionCoordinator) {
+        super.willTransition(to: newCollection, with: coordinator)
+        
+        // When the current trait collection changes (e.g. the device rotates),
+        // update the preferredContentSize.
+        self.updatePreferredContentSizeWithTraitCollection(newCollection)
+    }
+    
+    // Update its preferredContentSize
+    func updatePreferredContentSizeWithTraitCollection(_ traitCollection: UITraitCollection) {
+        
+        self.preferredContentSize = CGSize(width: self.view.bounds.size.width,
+                                           height: traitCollection.verticalSizeClass == .compact ? 270 : 400)
+        
+        // Dragging this slider updates
+        // the preferredContentSize of this view controller in real time.
+        //
+        // Update the slider with appropriate min/max values and reset the
+        // current value to reflect the changed preferredContentSize.
+        self.slider.maximumValue = Float(self.preferredContentSize.height)
+        self.slider.minimumValue = 220.0
+        self.slider.value = self.slider.maximumValue
+    }
+    
+    
+    // MARK: - Actions
+    
+    @IBAction func tipRateChanged(_ sender: UISegmentedControl) {
+        // when tip rate changed, fire a notification
+        
+        // 1 - 
+        let selectedIndex   = tipControl.selectedSegmentIndex
+        let selectedRate    = tipControl.titleForSegment(at: selectedIndex)
+        
+        // 2 - save tip rate to UserDefaults
+        let defaults = UserDefaults.standard
+        defaults.set(selectedIndex, forKey: "selectedIndex")
+        defaults.set(selectedRate, forKey: "selectedRate")
+        defaults.synchronize()
+        
+        // 3 -  notification data -- Only the index is sent!
+        let notificationData: [String:Int] = ["selectedIndex": selectedIndex]
+        // 4 - send notification
+        NotificationCenter.default.post(name: SETTINGS_CHANGED_NOTIFICATION,
+                                        object: self,
+                                        userInfo: notificationData)
+        
+    }
+    
+    @IBAction func sliderValueChanged(_ sender: UISlider) {
+        // print("Settings: Slider value changed to: \(sender.value)")
+        // change the height of this View Controller
+        self.preferredContentSize = CGSize(width: self.view.bounds.size.width,
+                                           height: CGFloat(sender.value))
+    }
+    
+    //| ----------------------------------------------------------------------------
+    //! Action for unwinding from the presented view controller (C).
+    //
+    @IBAction func unwindToCustomPresentationSecondViewController(_ sender: UIStoryboardSegue) {
     }
     
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
